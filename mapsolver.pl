0f42fb6d8ff8a2113fadfc11b9fed3dd08f8834e
Alex Schexnayder <alex.schex@gmail.com>
2023-12-05 20:15:28 -0600

    added a few predicate signatures to get us started.

--- mapsolver.pl
+++ mapsolver.pl
@@ -2,4 +2,16 @@
 % Student contributors: Grant Fernwaltt, Alex Schexnayder
 %% 
 
+find_exit(Maze,_).
+find_exit(Maze,Actions) :- 
 
+
+
+.
+
+
+ testSpace() :- % for testing the identity of current space position (wall vs floor vs start vs end)
+    . 
+
+ findStartSpace() :- % Locates Starting Position, if there is no start then maze is invalid.
+    . 
