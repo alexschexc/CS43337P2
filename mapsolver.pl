bfc6f65adb079427b423693dda22ba7bcbeef0c1
Alex Schexnayder <alex.schex@gmail.com>
2023-12-07 18:07:12 -0600

    Start position now reports correctly. End position reports row correctly, but not column.

--- mapsolver.pl
+++ mapsolver.pl
@@ -25,10 +25,10 @@ find_exit(Maze,Actions) :-
     %write('ymax is - '), write(Ymax),nl,
     %write('xmax is - '), write(Xmax),nl,
     write('locating start space'),nl,
-    findStartSpace(Maze, Begin, StartPosition),
+    findStartSpace(Begin, Maze, StartPosition),
     write('start space is in position '), write(StartPosition),nl,
     write('locating end space'),nl,
-    findEndSpace(Maze, Finish, EndPosition),
+    findEndSpace(Finish, Maze, EndPosition),
     write('end space is in position '), write(EndPosition),nl,
     write('testing maze').
     %write(Maze,' is a maze.').
@@ -45,49 +45,42 @@ find_exit(Maze,Actions) :-
 % Locates Starting Position, 
 % if there is no start or more 
 % than 1 start then maze is invalid.
-findStartSpace(Maze, Start, (X,Y)) :- 
-    write('1'),nl,
-    findStartInMaze(Maze,Start, X, 1, (X,Y)).
 
-findStartInMaze([], _, _, _, _):- fail.
-findStartInMaze([Row|Rest], Start, X, Y, (X,Y)) :-
-    write('a'),nl,
-    write((X,Y)),nl,
-    nth1(X,Row,Start),
+findStartInMaze(Start, [[RowHead|RestRow]|_],  X, 1) :-
+    nth1(X,[RowHead|RestRow],Start),
     !.
-findStartInMaze([_|Rest], Start, X, Y, (X,Y)) :-
-    write('b'),nl,
-    write(Y1),nl,
-    Y1 is Y + 1,
-    Y2 is Y1,
-    write('c'),nl,
-    write(Y2),nl,
-    findStartInMaze(Rest, Start, X, Y2, (X,Y2)).
+findStartInMaze(Start, [_|RestMatrix],  X, Y) :-
+    findStartInMaze(Start, [_|RestMatrix],  X, Y1),
+    Y is Y1 + 1.
+    
+% wrapper predicate
+findStart(Start, Maze, X, Y ) :- 
+    findStartInMaze(Start, Maze, X, Y).
+% wrapper for the wrapper, unifies X and Y variables to (X,Y) ordered pair.
+findStartSpace(Start, Maze, (X,Y)) :- 
+    findStart(Start, Maze, X, Y).
 
 % Locates Ending Position if no actions are given, 
 % if there is no end then maze is invalid.
-findEndSpace(Maze, End, (X,Y)) :- 
-    write('A'),nl,
-    findEndInMaze(Maze, End, X, 1, (X,Y)).
 
-findEndInMaze([], _, _, _, _) :- fail.
-findEndInMaze([Row|Rest], End, X, Y, (X,Y)) :- 
+findEndInMaze(End, [[RowHead|RestRow]|_],  X, 1) :- 
     write('I'),nl,
-    nth1(X, End, Row),
+    nth1(X,[RowHead|RestRow],End),
+    write(X),nl,
     !.
-findEndInMaze([_|Rest], End, X, Y, (X1,Y1)):-
+findEndInMaze(End, [_|RestMatrix], X, Y):-
     write('II'),nl,
-    Y2 is Y + 1,
-    findEndInMaze(Rest, End, X, Y2, (X1,Y1)).
+    findEndInMaze(End, [_|RestMatrix], X, Y2),
+    write('III'),nl,
+    Y is Y2 + 1.
+% wrapper predicate
+findEnd(End, Maze, X, Y) :- 
+    write('A'),nl,
+    findEndInMaze(End, Maze, X, Y).
+% wrapper for the wrapper, unifies X and Y variables to (X,Y) ordered pair.
+findEndSpace(End, Maze, (X,Y)) :-
+    findEnd(End, Maze, X, Y).
 
-findEndInRow([Space|_], End, X, Y) :-
-    write('a'),nl,
-    nth1(X,Space, End).
-findEndInRow([_|Rest], End, X, Y) :-
-    write('b'),nl,
-    X2 is X + 1,
-    write('c'),nl,
-    findEndInRow(Rest, End, X2, Y).
 % determines which adjacent spaces can be moved into
 %availableSpaces() :- .
 
